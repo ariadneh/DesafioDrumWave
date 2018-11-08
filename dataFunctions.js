5449c13c0815f5b7c8dabdc3b73f174488f4f9b3
ariadneh <ariadne0288@gmail.com>
2018-11-08 21:57:18 -0200

    gráficos prontos

--- dataFunctions.js
+++ dataFunctions.js
@@ -20,20 +20,65 @@ const minNumber = (array) => {
 }
 
 const numberData = (array) => {
-  return [media(array), maxNumber(array), minNumber(array)]
+  return [minNumber(array), maxNumber(array), media(array) ]
 }
+
 // ---------------------------------------------------------------------------
 // FUNÇÕES PARA COLUNAS CATEGÓRICAS
+function getFrequency(array) {
+  const uniqueValues = [...new Set(array)];
+  const numberUniqueValues = uniqueValues.length; 
+  console.log("Valores distintos:" + uniqueValues);
+  let numberAppearance = [];
 
+  uniqueValues.forEach((item, index) => {
+    if(typeof item === "string"){
+      numberAppearance[index] = countInArray(array, item);
+    }     
+  });
+  return [numberAppearance, uniqueValues];
+}
+
+function countInArray(array, what) {
+  return array.filter(item => item == what).length;
+}
+
+function categoryData(array) {
+  return [getFrequency()]
+}
 
 // ---------------------------------------------------------------------------
 // FUNÇÕES PARA COLUNAS DE TEXTO
 
-const numberOfWords = (string) => {
-  let words = string.split(' ');
-  return words.length - 1;
+function separateWordsInDescription(param) {
+ let words = [];
+ for (i = 0; i < param[0].length; i++) {
+   words.push(param[0][i].split(/[ \t\n\r\f\v\,\()\s\n\.\?\#\:]+/));
+ }
+ return words;
 }
 
-const numberOfChar = (string) => {
-  return string.length;
+function countWordsOfDescription(param) {
+ let sumArray = 0;
+ for (i = 0; i < param.length; i++) {
+   sumArray += param[i].length-1;
+ }
+ return sumArray;
+}
+
+function countCharacters(param) {
+ let words = [];
+ for (i = 0; i < param[0].length; i++) {
+   words.push(param[0][i].split(''));
+ }
+
+ let sumCharacters = 0;
+ for (i = 0; i < words.length; i++) {
+   sumCharacters += words[i].length;
+ }
+ return sumCharacters;
+}
+
+const textData = (array) => {
+  return [countWordsOfDescription(separateWordsInDescription(array)), countCharacters(array), 10]
 }
