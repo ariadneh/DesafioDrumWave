583aefc6f15689a6b690d2d6c66ca9102ba50448
ariadneh <ariadne0288@gmail.com>
2018-11-09 15:44:04 -0200

    .

--- sprint2.js
+++ sprint2.js
@@ -1,36 +1,61 @@
-window.onload = loadImages();
+window.onload = loadImagesModal1();
+window.onload = loadImagesModal2();
  
-function heroCharacteristics(indexHero) {
-  let dataDescription = ["Super Name", "Real Name", "Character Type", "Intelligence", "Strength", "Speed", "Durability", "Power", "Combat", "url"];
+ function heroCharacteristics(indexHero) {
+  let dataDescription = ["Super Name", "Real Name", "Character Type", "Intelligence", "Strength", "Speed", "Durability", "Power", "Combat"];
   let heroDescription = [];
-  for (i = 0; i < dataDescription.length - 1; i++) {
+  for (i = 0; i < dataDescription.length; i++) {
     heroDescription.push(getData(dataDescription[i])[0][indexHero]);
   }
+ return heroDescription;
+ }
 
-  return heroDescription;
-}
 
- function loadImages() {
-  let imagesContainer = document.getElementsByClassName("images-container");
+ function loadImagesModal1() {
+  let imagesContainer = document.getElementsByClassName("images-container1");
   images.forEach((imageURL, index) => {
     let div = $('<div>');
     let image = $('<img>');
-    $('.images-container').append(
+    $('.images-container1').append(
       $('<div>')
       .addClass('frame')
       .append($('<img>')
         .attr('src',imageURL)
         .attr('data-id', index)
-        .addClass('image'))
+        .addClass('image1'))
     );
+
+  });
+   let heroImage1 = document.getElementsByClassName("image1");
+   for (let i = 0; i < heroImage1.length; i++) {
+     heroImage1[i].addEventListener('click', showHeroes1, false);
+
+
+   }
+ }
+
+function loadImagesModal2() {
+  let imagesContainer = document.getElementsByClassName("images-container2");
+  images.forEach((imageURL, index) => {
+    let div = $('<div>');
+    let image = $('<img>');
+    $('.images-container2').append(
+    $('<div>')
+    .addClass('frame')
+    .append($('<img>')
+    .attr('src', imageURL)
+    .attr('data-id', index)
+    .addClass('image2'))
+    );
+
   });
 
-  let heroImage = document.getElementsByClassName("image");
-  for (let i = 0; i < heroImage.length; i++) {
-    heroImage[i].addEventListener('click', showHeroes, false);
+  let heroImage2 = document.getElementsByClassName("image2");
+  for (let i = 0; i < heroImage2.length; i++) {
+    heroImage2[i].addEventListener('click', showHeroes2, false);
+   
+
   }
-  document.getElementById("card1").addEventListener('click', () =>  document.getElementById("images-container").style.display = "flex");
-  document.getElementById("card2").addEventListener('click', () =>  document.getElementById("images-container").style.display = "flex");
 }
 
 $(".btn-fight").click(function() {
@@ -73,6 +98,13 @@ function grade(index){
 }
 
 function showHeroes(event) {
+  let index = event.target.dataset.id;
+  console.log(heroCharacteristics(index))
+
+}
+
+function showHeroes1(event) {
+ 
   let index = heroCharacteristics(event.target.dataset.id);
   document.getElementById("name1").textContent = index[0];
   document.getElementById("realName1").textContent = index[1];
@@ -83,5 +115,22 @@ function showHeroes(event) {
   document.getElementById("durability1").textContent = index[6];
   document.getElementById("power1").textContent = index[7];
   document.getElementById("combat1").textContent = index[8];
-  document.getElementById("image1").setAttribute('src', index[9]);
+
+}
+
+
+function showHeroes2(event) {
+
+  let index = heroCharacteristics(event.target.dataset.id);
+  document.getElementById("name2").textContent = index[0];
+  document.getElementById("realName2").textContent = index[1];
+  document.getElementById("characterType2").textContent = index[2];
+  document.getElementById("intelligence2").textContent = index[3];
+  document.getElementById("strength2").textContent = index[4];
+  document.getElementById("speed2").textContent = index[5];
+  document.getElementById("durability2").textContent = index[6];
+  document.getElementById("power2").textContent = index[7];
+  document.getElementById("combat2").textContent = index[8];
+
+
 }
