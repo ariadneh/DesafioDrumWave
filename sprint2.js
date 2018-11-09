071bb91183f772edf77b10ccf355d9effe1f287c
ariadneh <ariadne0288@gmail.com>
2018-11-09 10:18:14 -0200

    images

--- sprint2.js
+++ sprint2.js
@@ -1,53 +1,12 @@
-const getData = (input) => {
- let indexPosition = nomeColunas.findIndex((user, index) => user === input);
- return heroisTable.filter((user, index) => index === indexPosition);
-}
-
-const dropHeroMenu1 = document.getElementById("select-hero1");
-let hero1 = dropHeroMenu1.addEventListener("change", heroChoosen1);
-
-const dropHeroMenu2 = document.getElementById("select-hero2");
-let hero2 = dropHeroMenu2.addEventListener("change", heroChoosen2);
-
-window.onload = loadHeroMenu1();
-window.onload = loadHeroMenu2();
-//window.onload = loadImages();
-
-function loadHeroMenu1() {
-  const option = document.createElement("option");
-  option.innerHTML = "Herói 1";
-  option.value = "none";
-  dropHeroMenu1.appendChild(option);
-  for(hero1 of getData("Super Name")[0]){
-    const optionItem = document.createElement("option");
-    optionItem.innerHTML = hero1;
-    optionItem.value = hero1;
-    dropHeroMenu1.appendChild(optionItem);
-  }
-}
-
-function loadHeroMenu2() {
-  var option = document.createElement("option");
-  option.innerHTML = "Herói 2";
-  option.value = "none";
-  dropHeroMenu1.appendChild(option);
-  for(hero2 of getData("Super Name")[0]){
-    var optionItem = document.createElement("option");
-    optionItem.innerHTML = hero2;
-    optionItem.value = hero2;
-    dropHeroMenu2.appendChild(optionItem);
-  }
-}
+window.onload = loadImages();
 
 function heroChoosen1() {
   let hero1 = dropHeroMenu1.value;
-  console.log(hero1);
   return hero1;
 }
 
 function heroChoosen2() {
   let hero2 = dropHeroMenu2.value;
-  console.log(hero2);
   return hero2;
 }
 
@@ -56,12 +15,17 @@ function loadImages() {
   images.forEach((imageURL, index) => {
     let image = $('<img>');
     $('.images-container').append(
-      $('<img>')
-      .attr('src',imageURL)
-      .attr('data-id', index)
-      .addClass('image')
+      // $('<img>')
+      // .attr('src', imageURL)
+      // .attr('data-id', index)
+      // .addClass('image')
+      `<div class='frame'><img id='index' class='image' src=${imageURL}></div>`
     );
   });
+
+  for (let i = 0; i < imagesContainer.length; i++) {
+    imagesContainer[i].addEventListener('click', showHeroes, false);
+  }
 }
 
 $(".btn-fight").click(function() {
@@ -71,16 +35,17 @@ $(".btn-fight").click(function() {
 });
 
 function battleResult(hero1, hero2){
-  console.log(hero1, hero2);
   let indexHero1 = getData("Super Name")[0].findIndex(nameHero => nameHero === hero1);
   let indexHero2 = getData("Super Name")[0].findIndex(nameHero => nameHero === hero2);
-  console.log(indexHero1);
 
   let averageHero1 = grade(indexHero1);
   let averageHero2 = grade(indexHero2);
 
-  console.log(averageHero1);
-  //console.log(  getData('Intelligence')[0][indexHero1]); 
+  if (averageHero1 > averageHero2) {
+    return averageHero1;
+  } else {
+    return averageHero2;
+  }
 }
 
 function grade(index){
@@ -100,4 +65,10 @@ function grade(index){
   } 
   
   return sum;
+}
+
+function showHeroes(event) {
+  let index = event.target.dataset.id;
+
+
 }
